a2706efa5b7e5f7a9f97bbae60eef71bfe5b0f03
Christos-Angelopoulos <cangelopoulos@helb-prigogine.be>
2022-03-07 22:17:10 +0100

    Synchronisation des fichiers
    Synchronisation des fichiers

--- public/index.php
+++ public/index.php
@@ -4,8 +4,9 @@
 <title>Super Page</title>
 </head>
 <body>
-    <h1>Bonjour</h1>
-    <h1>JE SUIS FIER DE MOI</h1>
-    <h1>J'AI PASSE 15H SUR LE WEEK END A TRAVAILLER SUR DOCKER</h1>
+    
 </body>
+<script>
+    window.location.replace("../resources/views/Index.html");
+</script>
 </html>
