ab4bcf2b6983349c000ffd5fdffa9153dcaaed01
Patryk Krasucki <patryk.krasucki@helb-prigogine.be>
2022-03-10 22:27:44 +0100

    Version demo
    pushing p

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin</title>
+    @include('partials.navbar')
+</head>
+<body>
+    <h1>Page Admin</h1>
+</body>
+
+</html>
---
+++ resources/views/graphs.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Graphs</title>
+    @include('partials.navbar')
+</head>
+<body>
+    <h1>Graphs here</h1>
+</body>
+
+</html>
