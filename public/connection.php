9ff10d15dbb3b0896ab6656dfee82f58a145692f
Patryk Krasucki <100041611+patspoutnik2001@users.noreply.github.com>
2022-03-22 14:48:26 +0100

    Update connection.php

--- public/connection.php
+++ public/connection.php
@@ -4,16 +4,16 @@
 
 // on défini les paramètre de connexion à la DB
 //**************   local   */
-$host='127.0.0.1';
-$user='root';
-$pass='';
-$db='helb_covidsaferoom';
+#$host='127.0.0.1';
+#$user='root';
+#$pass='';
+#$db='helb_covidsaferoom';
 
 //***************   server */
-#$host='mysql';
-#$user='default';
-#$pass='lkiawsdkjsdfbkjzsdlkjasdf';
-#$db='helb_covidsaferoom';
+$host='mysql';
+$user='default';
+$pass='lkiawsdkjsdfbkjzsdlkjasdf';
+$db='helb_covidsaferoom';
 
 // Définition des variables de connexion
 $dsn = "mysql:host={$host};dbname={$db}";
