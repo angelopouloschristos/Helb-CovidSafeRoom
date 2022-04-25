ffb490a94a11cf9b6fe9ef3015cb74092199943b
Patryk <patryk.krasucki@helb-prigogine.be>
2022-04-25 22:31:38 +0200

    bablabalbbla

--- public/js/element.loader.js
+++ public/js/element.loader.js
@@ -5,7 +5,7 @@ window.onload = function ()
     header.innerHTML = "<header>\n" +
         "    <nav class=\"navbar navbar-expand-lg navbar-dark bg-primary\">\n" +
         "        <div class=\"container-fluid\">\n" +
-        "            <image src=\"https://i.imgur.com/7Y7kaqF.png\" width=\"auto\" height=\"35\"></image><a class=\"navbar-brand\" href=\"graphs.php\">CSR</a>\n" +
+        "            <image src=\"https://i.imgur.com/7Y7kaqF.png\" width=\"auto\" height=\"35\"></image><a class=\"navbar-brand\" href=\"index.php\">CSR</a>\n" +
         "            <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarColor01\" aria-controls=\"navbarColor01\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
         "                <span class=\"navbar-toggler-icon\"></span>\n" +
         "            </button>\n" +
@@ -13,7 +13,7 @@ window.onload = function ()
         "            <div class=\"collapse navbar-collapse\" id=\"navbarColor01\">\n" +
         "                <ul class=\"navbar-nav me-auto\">\n" +
         "                    <li class=\"nav-item\">\n" +
-        "                        <a class=\"nav-link active\" href=\"graphs.php\">Home\n" +
+        "                        <a class=\"nav-link active\" href=\"index.php\">Home\n" +
         "                            <span class=\"visually-hidden\">(current)</span>\n" +
         "                        </a>\n" +
         "                    </li>\n" +
